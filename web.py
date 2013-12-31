89e94a9d873fbad655e14e33c9c7e3730f290db3
njdup <njdupoux@stanford.edu>
2013-12-31 12:57:46 -0800

    Converted about, membership, and outreach pages

--- web.py
+++ web.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/membership')
+def membership():
+	return render_template('membership.html')
+
+@app.route('/outreach')
+def outreach():
+	return render_template('outreach.html')
+	
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
